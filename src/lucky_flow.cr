36d35daf22baf70e65624d455115ffadd6293f40
Paul Smith <paulcsmith0218@gmail.com>
2018-04-13 10:47:16 -0400

    Initial commit

---
+++ src/lucky_flow.cr
@@ -0,0 +1,6 @@
+require "./lucky_flow/*"
+
+# TODO: Write documentation for `LuckyFlow`
+module LuckyFlow
+  # TODO: Put your code here
+end
---
+++ src/lucky_flow/version.cr
@@ -0,0 +1,3 @@
+module LuckyFlow
+  VERSION = "0.1.0"
+end
