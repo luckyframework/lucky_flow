924bf74c08fb47063d098da8b8f1d83865d588d2
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-06-02 20:12:59 -0500

    Create HaveTextExpectation

---
+++ spec/lucky_flow/expectations/have_text_expectation_spec.cr
@@ -0,0 +1,54 @@
+require "../../spec_helper"
+
+module LuckyFlow::Expectations
+  describe HaveTextExpectation do
+    describe "#match" do
+      it "returns true if element has text" do
+        element = LuckyFlow::Element.new(raw_selector: "@heading")
+        
+        visit_page_with "<span flow-id='heading'>Hello, World!</span>"
+
+        expectation = HaveTextExpectation.new(expected_value: "Hello, World!")
+        
+        expectation.match(element).should be_true
+      end
+
+      it "returns false if element does not have text" do
+        element = LuckyFlow::Element.new(raw_selector: "@heading")
+        
+        visit_page_with "<span flow-id='heading'>Hello, World!</span>"
+
+        expectation = HaveTextExpectation.new(expected_value: "Hello, Steve!")
+        
+        expectation.match(element).should be_false
+      end
+    end
+
+    describe "#failure_message" do
+      it "returns actual text found on element" do
+        element = LuckyFlow::Element.new(raw_selector: "@heading")
+
+        visit_page_with "<span flow-id='heading'>Hello, World!</span>"
+
+        expectation = HaveTextExpectation.new(expected_value: "Hello, Steve!")
+        message = expectation.failure_message(element)
+        
+        message.should contain("Expected element to have text: Hello, Steve!")
+        message.should contain("actual: Hello, World!")
+      end
+    end
+
+    describe "#negative_failure_message" do
+      it "returns actual text found on element" do
+        element = LuckyFlow::Element.new(raw_selector: "@heading")
+
+        visit_page_with "<span flow-id='heading'>Hello, World!</span>"
+
+        expectation = HaveTextExpectation.new(expected_value: "Hello, World!")
+        message = expectation.negative_failure_message(element)
+        
+        message.should contain("Expected element not to have text: Hello, World!")
+      end
+    end
+  end
+end
---
+++ src/lucky_flow/expectations/have_text_expectation.cr
@@ -0,0 +1,19 @@
+struct LuckyFlow::Expectations::HaveTextExpectation
+  def initialize(@expected_value : String)
+  end
+
+  def match(element)
+    element.text == @expected_value
+  end
+
+  def failure_message(element)
+    <<-MSG
+    Expected element to have text: #{@expected_value}
+                            actual: #{element.text}
+    MSG
+  end
+
+  def negative_failure_message(element)
+    "Expected element not to have text: #{@expected_value}"
+  end
+end
