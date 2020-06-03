c7b7d65b21cffc7fd569ccec108ee8b2caf8b3c0
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-06-02 19:45:21 -0500

    Extract BeOnPageExpectation into its own file

--- src/lucky_flow/expectations.cr
+++ src/lucky_flow/expectations.cr
@@ -1,28 +1,7 @@
+require "./expectations/**"
+
 class LuckyFlow
   module Expectations
-    struct BeOnPageExpectation
-      def match(element : LuckyFlow::Element) : Bool
-        element.displayed?
-      rescue LuckyFlow::ElementNotFoundError
-        false
-      end
-
-      def failure_message(element)
-        LuckyFlow::ErrorMessageWhenNotFound.build(
-          element.selector,
-          element.inner_text
-        )
-      end
-
-      def negative_failure_message(element)
-        LuckyFlow::ErrorMessageWhenNotFound.build(
-          element.selector,
-          element.inner_text,
-          negate: true
-        )
-      end
-    end
-
     private def be_on_page
       BeOnPageExpectation.new
     end
