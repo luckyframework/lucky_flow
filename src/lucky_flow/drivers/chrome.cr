fdfb3bff1aa103a59af1209f91d1324b01e6d84c
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-12-30 17:00:06 -0500

    Allow running Chrome headfully Fixes #110

---
+++ src/lucky_flow/drivers/chrome.cr
@@ -0,0 +1,30 @@
+abstract class LuckyFlow::Drivers::Chrome < LuckyFlow::Driver
+  def start_session : Selenium::Session
+    driver.create_session(capabilities)
+  rescue e : IO::Error
+    retry_start_session(e)
+  end
+
+  def stop
+    driver.stop
+  end
+
+  protected def capabilities
+    Selenium::Chrome::Capabilities.new
+  end
+
+  @driver : Selenium::Driver?
+
+  private def driver : Selenium::Driver
+    @driver ||= begin
+      service = Selenium::Service.chrome(driver_path: driver_path)
+      Selenium::Driver.for(:chrome, service: service)
+    end
+  end
+
+  private def driver_path
+    LuckyFlow.settings.chromedriver_path || Webdrivers::Chromedriver.install
+  rescue err
+    raise DriverInstallationError.new(err)
+  end
+end
--- src/lucky_flow/server.cr
+++ src/lucky_flow/server.cr
@@ -3,56 +3,18 @@ class LuckyFlow::Server
   # This is used so that only one server instance is started
   INSTANCE = new
 
-  CAPABILITIES = Selenium::Chrome::Capabilities.new
-  CAPABILITIES.args(["no-sandbox", "headless", "disable-gpu"])
-
-  @retry_limit : Time?
   @session : Selenium::Session?
-  @driver : Selenium::Driver?
+  private getter driver : LuckyFlow::Driver = LuckyFlow::Drivers::HeadlessChrome.new
 
   # Use LuckyFlow::Server::INSTANCE instead
   private def initialize
   end
 
-  # Start a new selenium session with Chromedriver
+  # Start a new selenium session
   def session : Selenium::Session
-    @session ||= create_session
-  end
-
-  private def driver : Selenium::Driver
-    @driver ||= begin
-      service = Selenium::Service.chrome(driver_path: driver_path)
-      Selenium::Driver.for(:chrome, service: service)
-    end
-  end
-
-  private def create_session : Selenium::Session
-    @retry_limit = 2.seconds.from_now
-    prepare_screenshot_directory
-    start_session
-  end
-
-  # If less than 0.34.0
-  {% if compare_versions(Crystal::VERSION, "0.34.0") == -1 %}
-    private def start_session
-      driver.create_session(CAPABILITIES)
-    rescue e : Errno
-      retry_start_session(e)
-    end
-  {% else %}
-    private def start_session
-      driver.create_session(CAPABILITIES)
-    rescue e : IO::Error
-      retry_start_session(e)
-    end
-  {% end %}
-
-  private def retry_start_session(e)
-    if Time.utc <= @retry_limit.not_nil!
-      sleep(0.1)
-      start_session
-    else
-      raise e
+    @session ||= begin
+      prepare_screenshot_directory
+      driver.start_session
     end
   end
 
@@ -65,35 +27,12 @@ class LuckyFlow::Server
     LuckyFlow.settings.screenshot_directory
   end
 
-  private def browser_binary
-    LuckyFlow.settings.browser_binary
-  end
-
-  private def capabilities
-    if browser_binary.nil?
-      CAPABILITIES
-    else
-      CAPABILITIES.merge({
-        chromeOptions: {
-          args:   CAPABILITIES[:chromeOptions][:args],
-          binary: browser_binary,
-        },
-      })
-    end
-  end
-
   def reset
     @session.try &.cookie_manager.delete_all_cookies
   end
 
   def shutdown
     @session.try &.delete
-    @driver.try &.stop
-  end
-
-  private def driver_path
-    LuckyFlow.settings.chromedriver_path || Webdrivers::Chromedriver.install
-  rescue err
-    raise DriverInstallationError.new(err)
+    @driver.stop
   end
 end
