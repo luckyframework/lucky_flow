ff7c22d3c72365ededf7bc677669ed95264c2f37
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2021-01-06 11:28:18 -0500

    Add back support for browser_binary setting (#114)

--- src/lucky_flow/drivers/chrome.cr
+++ src/lucky_flow/drivers/chrome.cr
@@ -1,5 +1,8 @@
 abstract class LuckyFlow::Drivers::Chrome < LuckyFlow::Driver
   def start_session : Selenium::Session
+    capabilities = Selenium::Chrome::Capabilities.new
+    capabilities.chrome_options.args = args
+    capabilities.chrome_options.binary = browser_binary
     driver.create_session(capabilities)
   rescue e : IO::Error
     retry_start_session(e)
@@ -9,8 +12,8 @@ abstract class LuckyFlow::Drivers::Chrome < LuckyFlow::Driver
     @driver.try(&.stop)
   end
 
-  protected def capabilities
-    Selenium::Chrome::Capabilities.new
+  protected def args : Array(String)
+    [] of String
   end
 
   @driver : Selenium::Driver?
@@ -26,5 +29,9 @@ abstract class LuckyFlow::Drivers::Chrome < LuckyFlow::Driver
     LuckyFlow.settings.driver_path || Webdrivers::Chromedriver.install
   rescue err
     raise DriverInstallationError.new(err)
+  end
+
+  private def browser_binary : String?
+    LuckyFlow.settings.browser_binary
   end
 end
